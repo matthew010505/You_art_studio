289c6442aff5ba0322f124ab15840692781ee294
matthew010505 <117917780+matthew010505@users.noreply.github.com>
2024-02-13 22:49:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".header").addEventListener('loadstart', function() {
+    // Get the audio element
+    var audio = new Audio('hukum.mp3');
+    // Play the audio
+    audio.play();
+});
